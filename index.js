904ed59aac11898065c20415d85b2639f95a2114
J.D. Bean <jonathandbean@gmail.com>
2017-12-13 16:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function moveDodgerRight() {
    var rightNumbers = dodger.style.left.replace('px', '')
    var right = parseInt(rightNumbers, 10) + 40
 
-   dodger.style.right = `${right - 4}px`
+   dodger.style.left = `${left + 4}px`
 
    if (right > GAME_WIDTH) {
      window.requestAnimationFrame(moveDodgerRight)
